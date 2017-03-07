bd28e8ec89b2694b9f9cc1302911618687449b12
RyodoTanaka <groadpg@gmail.com>
2017-03-08 01:46:57 +0900

    [add] setup script

---
+++ CPP/NAIST/setup.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# ENV PATH
+export OMPL_APPS_DIR=`pwd`
+# List of the Execution files
+EXE_FILES=$(ls ./bin)
+
+# This Script's Path
+SCRIPT_PATH=$(dirname ${BASH_SOURCE:-$0})
+
+function _func_complete_omplrun()
+{
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    if [ "$COMP_CWORD" -eq 1 ]; then
+        COMPREPLY=( $(compgen -W ${EXE_FILES} -- $cur) )
+fi
+}
+
+function func_ompl()
+{
+    `find ${SCRIPT_PATH} -name $1`
+}
+
+alias omplrun=func_ompl
+complete -o default -F _func_complete_omplrun omplrun
