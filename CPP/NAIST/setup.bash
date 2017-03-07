798057c75ccdb183bd4d96c3318c3cb256f7579b
RyodoTanaka <groadpg@gmail.com>
2017-03-08 02:43:17 +0900

    [fix] path

--- CPP/NAIST/setup.bash
+++ CPP/NAIST/setup.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# ENV PATH
-export OMPL_APPS_DIR=`pwd`
-
 # This Script's Path
 SCRIPT_PATH=$( dirname ${BASH_SOURCE:-$0} )
+
+# ENV PATH
+export OMPL_APPS_DIR=${SCRIPT_PATH}
+export OMPL_APPS_EXE_FILES=$(ls ${SCRIPT_PATH}/bin)
 
 # List of the Execution files
 EXE_FILES=$(ls ${SCRIPT_PATH}/bin)
@@ -13,7 +14,7 @@ function _func_complete_omplrun()
 {
     local cur=${COMP_WORDS[COMP_CWORD]}
     if [ "$COMP_CWORD" -eq 1 ]; then
-        COMPREPLY=( $(compgen -W ${EXE_FILES} -- $cur) )
+        COMPREPLY=( $(compgen -W $OMPL_APPS_EXE_FILES -- $cur) )
 fi
 }
 
