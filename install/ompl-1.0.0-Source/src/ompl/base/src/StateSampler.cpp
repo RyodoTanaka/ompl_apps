f64f813afe4f1286e5969703d9b727a781803662
RyodoTanaka <groadpg@gmail.com>
2017-04-03 13:24:38 +0900

    [modify] delete comment fase

--- install/ompl-1.0.0-Source/src/ompl/base/src/StateSampler.cpp
+++ install/ompl-1.0.0-Source/src/ompl/base/src/StateSampler.cpp
@@ -56,11 +56,10 @@ void ompl::base::CompoundStateSampler::sampleUniformNear(State *state, const Sta
   State **comps = state->as<CompoundState>()->components;
   State **nearComps = near->as<CompoundState>()->components;
   for (unsigned int i = 0 ; i < samplerCount_ ; ++i){
-	std::cout << "weightInportance[" << i << "] : " << weightImportance_[i] << std::endl;
-	  if (weightImportance_[i] > std::numeric_limits<double>::epsilon())
-		samplers_[i]->sampleUniformNear(comps[i], nearComps[i], distance * weightImportance_[i]);
-	  else
-		samplers_[i]->sampleUniform(comps[i]);
+    if (weightImportance_[i] > std::numeric_limits<double>::epsilon())
+      samplers_[i]->sampleUniformNear(comps[i], nearComps[i], distance * weightImportance_[i]);
+    else
+      samplers_[i]->sampleUniform(comps[i]);
   }
 
 }
