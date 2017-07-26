168d3a1f5287077bd8a8e86a2cdd4bd08b2ccac5
RyodoTanaka <groadpg@gmail.com>
2017-07-27 04:19:10 +0900

    [modify] compile script
    moved setup script from compile.bash to build.bash

--- CPP/Practice/compile.bash
+++ CPP/Practice/compile.bash
@@ -7,7 +7,6 @@ if [ "$?" -eq 0 ]; then
     notify-send -i $(pwd)/../../.images/ompl-success.jpg -t 1000 'OMPL LIB. COMPILER' ' (｀・ω・´) yes!! OMPL-1.0.0 code SUCCEEDED!'
     source build.bash clean
     source build.bash
-    source setup.bash
 else
     notify-send -i $(pwd)/../../.images/ompl-fail.jpg -t 1000 'OMPL LIB. COMPILER' '(´･ω ･`) oops... OMPL-1.0.0 code FAILED...' 
 fi
