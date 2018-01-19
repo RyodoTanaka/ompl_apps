2f0f2b9ea4cd20e72eb7314b65434959d851f838
RyodoTanaka <k104073r@mail.kyutech.jp>
2018-01-19 18:15:51 +0900

    [modify] ompl compiling path

--- CPP/Practice/compile.bash
+++ CPP/Practice/compile.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo make -j`nproc` -C ../../install/ompl-1.0.0-Source/build/Release/ uninstall
-sudo make -j`nproc` -C ../../install/ompl-1.0.0-Source/build/Release/ install
+sudo make -j`nproc` -C ../../install/ompl/build/Release/ uninstall
+sudo make -j`nproc` -C ../../install/ompl/build/Release/ install
 
 if [ "$?" -eq 0 ]; then
     notify-send -i $(pwd)/../../.images/ompl-success.jpg -t 1000 'OMPL LIB. COMPILER' ' (｀・ω・´) yes!! OMPL-1.0.0 code SUCCEEDED!'
