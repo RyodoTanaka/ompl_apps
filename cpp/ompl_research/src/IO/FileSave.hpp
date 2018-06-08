909b13df3cb97edba39bd039c03354ac07c33867
RyodoTanaka <groadpg@gmail.com>
2018-06-08 17:50:00 +0900

    [delete] submodule directory

--- cpp/ompl_research/src/IO/FileSave.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef __FILE_SAVE_HPP__
-#define __FILE_SAVE_HPP__
-
-std::string setFilename(const std::string dir_name);
-std::string setFilename(const std::string dir_name, const std::string file_name);
-
-#endif
