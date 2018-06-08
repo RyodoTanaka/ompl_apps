dd1cacc9e035956a3ec83f953f164dd76937e2d1
RyodoTanaka <groadpg@gmail.com>
2018-06-08 11:15:01 +0900

    [modify] renamed file configuration

---
+++ cpp/ompl_research/src/IO/FileSave.hpp
@@ -0,0 +1,7 @@
+#ifndef __FILE_SAVE_HPP__
+#define __FILE_SAVE_HPP__
+
+std::string setFilename(const std::string dir_name);
+std::string setFilename(const std::string dir_name, const std::string file_name);
+
+#endif
