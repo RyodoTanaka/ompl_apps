80656d809ddca4f9a09a21286f218becebf9e86d
RyodoTanaka <groadpg@gmail.com>
2018-06-08 17:25:46 +0900

    [modify] add some option for compile

--- cpp/example/compile.bash
+++ cpp/example/compile.bash
@@ -2,23 +2,31 @@
 
 CURRENT=`pwd`
 
-if [ "$1" == "clean" ]; then
+if [ "$1" == "init" ]; then
     echo "Rebuild the OMPL Library..."
-    rm -rf ../../install/ompl/build/Release/*
+    if [ -e ../../install/ompl/build/Release ]; then
+        rm -rf ../../install/ompl/build/Release/*
+    else
+        mkdir -p ../../install/ompl/build/Release
+    fi
     cd ../../install/ompl/build/Release
     cmake ../..
-    cd $CURRENT
-    sudo make -j`nproc` -C ../../install/ompl/build/Release/ install
+    sudo make -j`nproc` install
     if [ "$?" -eq 0 ]; then
         shift
         notify-send -i $(pwd)/../../.images/ompl-success.jpg -t 1000 'OMPL LIB. COMPILER' ' (｀・ω・´) yes!! OMPL Lib COMPILED!'
+        cd $CURRENT
     else
         notify-send -i $(pwd)/../../.images/ompl-fail.jpg -t 1000 'OMPL LIB. COMPILER' '(´･ω ･`) oops... OMPL Lib NOT COMPILED...'
+        cd $CURRENT
         return $?
     fi
-else
+elif [ "$1" == "install" ]; then
+    echo "Installation will start."
     sudo make -j`nproc` -C ../../install/ompl/build/Release/ uninstall
     sudo make -j`nproc` -C ../../install/ompl/build/Release/ install
+else
+    echo "Normal compile will start."
 fi
 
 if [ "$?" -eq 0 ]; then
